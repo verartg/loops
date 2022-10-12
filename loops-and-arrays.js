7694a24d44316d7654d082dd2f433b400c8db758
verartg <verarottger@gmail.com>
2022-10-12 14:39:31 +0200

    Huiswerk af

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -3,17 +3,17 @@
 // Schrijf een script dat iedere naam in onderstaande array vervangt door een koosnaampje, door er -"je" achter te plakken.
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
 // ==========================================
-
-const names = ["Henk", "Piet", "Fred", "Joop"];
-
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
+const names = ["Henk", "Piet", "Fred", "Joop"];
 
-
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i] = names[i] + "je");
+}
 
 // ==========================================
 // Opdracht 2
@@ -21,17 +21,23 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
 // ==========================================
 
-const numbers = [2, 4, 5, 29, 38];
-
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(numbers) geeft: [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
 
+const numbers = [2, 4, 5, 29, 38];
 
-
-
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] === 0) {
+        console.log(numbers[i] * 2);
+    } else if (numbers[i] % 2 === 0) {
+        console.log(numbers[i] * 2);
+    } else {
+        console.log(numbers[i] * 3);
+    }
+}
 // ==========================================
 // Opdracht 3
 // Schrijf een script dat voor de maat van ieder vierkant in onderstaande array het volume uitrekent.
@@ -39,8 +45,6 @@ const numbers = [2, 4, 5, 29, 38];
 // Het volume is lengte x breedte x hoogte
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
 // ==========================================
-
-const squares = [30, 2, 8, 24, 11];
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -53,3 +57,11 @@ const squares = [30, 2, 8, 24, 11];
 //   'Het volume van 24 is 13824',
 //   'Het volume van 11 is 1331'
 // ]
+
+const squares = [30, 2, 8, 24, 11];
+
+console.log(squares);
+for (let i = 0; i < squares.length; i++) {
+    console.log("Het volume van " + squares[i], "is " + (squares[i]= Math.pow(squares[i],3)));
+}
+console.log(squares);
